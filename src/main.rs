8d68ce033f2c325f846624b152003575b4ac86e3
Wang Jian <wangjian0504@gmail.com>
2024-04-12 20:43:52 +0800

    分离命令行解析

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let (query, file_path) = parse_config(&args);
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    (query, file_path)
 }
