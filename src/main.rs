4245c7c08f1af438c19648f69ec04d27abe0286a
Wang Jian <wangjian0504@gmail.com>
2024-04-14 14:57:00 +0800

    分离逻辑代码到库包中

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,19 @@
-use std::{env, fs};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("配置错误: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
+    if let Err(e) = minigrep::run(config) {
+        println!("程序错误: {e}");
+        process::exit(1);
     }
 }
